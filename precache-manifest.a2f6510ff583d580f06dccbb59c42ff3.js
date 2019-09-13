51bd1fe331db7849d858d439cd700e3437762419
unknown <shklyarigor2015@gmail.com>
2019-09-13 18:16:02 +0300

    Updates

---
+++ precache-manifest.a2f6510ff583d580f06dccbb59c42ff3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2bf01f8c501afb8c08b5602ba7178708",
+    "url": "/react_tabs/index.html"
+  },
+  {
+    "revision": "bc879684a4eece5aee59",
+    "url": "/react_tabs/static/css/main.a5f7d18d.chunk.css"
+  },
+  {
+    "revision": "9c5ca2f4a5e34b4fd994",
+    "url": "/react_tabs/static/js/2.be491123.chunk.js"
+  },
+  {
+    "revision": "bc879684a4eece5aee59",
+    "url": "/react_tabs/static/js/main.921d6b9e.chunk.js"
+  },
+  {
+    "revision": "010ecae327fc4474afd9",
+    "url": "/react_tabs/static/js/runtime~main.310f5775.js"
+  }
+]);
